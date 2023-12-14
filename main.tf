687dada2c975657c9636a5627ae29635c6932281
bhanu <bhanusaireddy5244@gmail.com>
2023-12-14 13:00:15 +0530

    terraform.

--- main.tf
+++ main.tf
@@ -15,13 +15,13 @@ resource "aws_internet_gateway" "igw" {
     Name = "main"
   }
 }
-#resource "aws_route" "igw" {
- # for_each = lookup(lookup(module.subnets, "public",null), "route_table_id", null)
-  #route_table_id            = each.value["id"]
-  #destination_cidr_block    = "0.0.0.0/0"
-  #gateway_id = aws_internet_gateway.igw.id
+resource "aws_route" "igw" {
+ for_each = lookup(lookup(lookup(module.subnets, "public",null), "route_table", null),"id",null)
+  route_table_id            = each.value["id"]
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
 
-#}
+}
 
 output "subnet" {
   value = module.subnets
