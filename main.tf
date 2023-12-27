cb173f3ae4e10861b437bcadbd8f1d5a5967f3a0
bhanu <bhanusaireddy5244@gmail.com>
2023-12-27 21:37:04 +0530

    terraform.

--- main.tf
+++ main.tf
@@ -43,11 +43,11 @@ resource "aws_nat_gateway" "ngw" {
   }
 
 
-  resource "aws_vpc_peering_connection" "peering" {
-  peer_vpc_id   = aws_vpc.main.id
-  vpc_id        = var.default_vpc_id
-    auto_accept   = true
-}
+  #resource "aws_vpc_peering_connection" "peering" {
+  #peer_vpc_id   = aws_vpc.main.id
+  #vpc_id        = var.default_vpc_id
+    #auto_accept   = true
+#}
 
 
 
