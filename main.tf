5203172426ac478037b961e6d5444008cbe7e9b3
bhanu <bhanusaireddy5244@gmail.com>
2023-12-27 22:07:10 +0530

    terraform.

--- main.tf
+++ main.tf
@@ -47,12 +47,6 @@ output "subnet" {
 }
 
 
-#resource "aws_vpc_peering_connection" "peering" {
-  #peer_vpc_id = aws_vpc.main.id
-  #vpc_id      = var.default_vpc_id
-  #auto_accept = true
-
-#}
 
 
 
--- subnets/vars.tf
+++ subnets/vars.tf
@@ -1,3 +1,2 @@
 variable "subnets" {}
 variable "vpc_id" {}
-#variable "default_vpc_id" {}
