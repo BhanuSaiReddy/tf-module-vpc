70867655a927569473bb1a10d3ead0c51b4a4266
bhanu <bhanusaireddy5244@gmail.com>
2023-12-27 21:22:22 +0530

    terraform.

--- main.tf
+++ main.tf
@@ -41,14 +41,12 @@ resource "aws_nat_gateway" "ngw" {
     nat_gateway_id = element(aws_nat_gateway.ngw.*.id, count.index )
 
   }
-output "subnet" {
-  value = "module.subnets"
-}
 
-   resource "aws_vpc_peering_connection" "peering" {
+
+  resource "aws_vpc_peering_connection" "peering" {
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.default_vpc_id
-     auto_accept   = true
+    auto_accept   = true
 }
 
 
