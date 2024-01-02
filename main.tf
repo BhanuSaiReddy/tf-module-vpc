06856803fa93fb8ba1f59cb79d9e0138ecebf490
bhanu <bhanusaireddy5244@gmail.com>
2024-01-02 16:32:36 +0530

    terraform.

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_route" "peer" {
   count             = length(local.private_route_table_ids)
   route_table_id         = element(local.private_route_table_ids , count.index )
   destination_cidr_block = var.default_vpc_cidr
-  vpc_peering_connection_id = "aws_vpc_peering_connection.peering.id"
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 
 }
 
@@ -61,7 +61,7 @@ resource "aws_route" "peer" {
 resource "aws_route" "default-vpc-peer-entry" {
   route_table_id         = var.default_vpc_route_table_id
   destination_cidr_block = var.cidr
-  vpc_peering_connection_id = "aws_vpc_peering_connection.peering.id"
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 
 }
 
