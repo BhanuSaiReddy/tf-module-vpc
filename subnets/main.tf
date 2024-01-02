ccda645dafd60498c8ed748b275ba857f625552e
bhanu <bhanusaireddy5244@gmail.com>
2024-01-02 20:09:32 +0530

    terraform.

--- subnets/main.tf
+++ subnets/main.tf
@@ -10,13 +10,11 @@ resource "aws_route_table" "main" {
   for_each = var.subnets
   vpc_id = var.vpc_id
   tags = merge(var.tags, { Name = "${var.env}-${each.key}-rt" })
-  }
 }
+
 
 resource "aws_route_table_association" "a" {
-  for_each = var.subnets
-  subnet_id      = lookup(lookup(aws_subnet.main,each.key, null ), "id",null)
-  route_table_id = lookup(lookup(aws_route_table.main,each.key, null ), "id",null)
+for_each = var.subnets
+subnet_id      = lookup(lookup(aws_subnet.main,each.key, null ), "id",null)
+route_table_id = lookup(lookup(aws_route_table.main,each.key, null ), "id",null)
 }
-
-
