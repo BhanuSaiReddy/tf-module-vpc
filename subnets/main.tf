1e04e7afc4d75752fe6650e8b4d1beb51a0794e1
bhanu <bhanusaireddy5244@gmail.com>
2024-01-02 19:59:39 +0530

    terraform.

--- subnets/main.tf
+++ subnets/main.tf
@@ -5,6 +5,7 @@ resource "aws_subnet" "main" {
   availability_zone = each.value["az"]
   tags = merge(var.tags, { Name = "${var.env}-${each.key}-subnet" })
 
+}
 resource "aws_route_table" "main" {
   for_each = var.subnets
   vpc_id = var.vpc_id
